6824fcd4feba3ccfba0fe190b9dcce32230bad7e
gzbfgjf2 <142939562+gzbfgjf2@users.noreply.github.com>
2024-01-30 12:00:00 +0000

    Prepare vite deployment for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: `assets/[name][extname]`,
-        entryFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name][extname]`
-      },
-    },
-  },
+  base: "/visx-drag-data/",
 });
